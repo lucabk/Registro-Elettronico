dcdc9ebcf23a2ebb5bd8454e2a0a61ba5bc5318f
lucabk <lucabk96l@gmail.com>
2025-05-30 19:00:24 +0200

    creazione del progetto spring boot

---
+++ src/main/java/it/registro/scuola/RegistroElettronicoApplication.java
@@ -0,0 +1,13 @@
+package it.registro.scuola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroElettronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroElettronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/registro/scuola/RegistroElettronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.registro.scuola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroElettronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
