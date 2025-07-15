49260f076b5d6e27f69b4e75e3704d99a409c5c6
lucabk <lucabk96l@gmail.com>
2025-07-15 17:59:55 +0200

    aggiunto interceptor axios per gestione errori 401 e 403 di risposta dal server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,21 @@ import Login from './components/Auth/Login'
 import UserContext from './components/context/userContext'
 import { useContext, useEffect } from 'react'
 import { toast } from 'react-toastify'
+import { setUserDispatcher } from './service/axiosConfig'
 
 const App = () => {
   const [user, userDispatcher] = useContext(UserContext)
 
   //fetch browser local storage: ripopola il contesto globale (userToken) con il token salvato nella memoria del browser al refresh
   useEffect(() => {
+    setUserDispatcher(userDispatcher) //axios interceptor
     const token = window.localStorage.getItem("token")
     if (token) {
         const tokenPayload = JSON.parse(atob(token.split(".")[1]))
         if (Math.floor(Date.now()/1000) < tokenPayload.exp){
           userDispatcher({ type : "SAVE_USER", payload : token })
         } else {                                                    //token expired
-          toast.error("JWT Token expired, please Login")
+          toast.error("Sessione scaduta. Si prega di effettuare il login")
           window.localStorage.removeItem("token")
           userDispatcher({ type : "DELETE_USER" })
         }
--- frontend/src/service/axiosConfig.js
+++ frontend/src/service/axiosConfig.js
@@ -1,9 +1,16 @@
 import axios from "axios"
+import { toast } from "react-toastify"
+
+let userDispatcher = null // variabile per gestire lo userDispatcher
+export const setUserDispatcher = (dispatcher) => {
+    userDispatcher = dispatcher
+}
 
 const instance = axios.create({
     baseURL: "http://localhost:8080", 
 })
 
+// Interceptor richiesta → aggiunge token
 instance.interceptors.request.use((req) => {
     const token = localStorage.getItem("token")
     if (token) {
@@ -11,5 +18,20 @@ instance.interceptors.request.use((req) => {
     }
     return req
 })
+
+// Interceptor risposta → intercetta errori 
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            toast.error("Sessione scaduta o non autorizzata. Effettua di nuovo il login.")
+            localStorage.removeItem("token")
+            if (userDispatcher) {
+                userDispatcher({ type: "DELETE_USER" })
+            }
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default instance
