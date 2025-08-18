3d46e88c042dd2361e5abbef5d8f2f4292d8ae8d
lucabk <lucabk96l@gmail.com>
2025-08-18 16:42:34 +0200

    front-end aggiunto calendario compiti

--- frontend/src/components/Teacher/TeacherSchool.jsx
+++ frontend/src/components/Teacher/TeacherSchool.jsx
@@ -37,13 +37,26 @@ const TeacherSchool = ({ teacherId, incarichi, setIncarichi }) => {
                                         <td className="text-center">{i.scuolaDTO.citta}</td>
                                         <SingleIncarico incarico={i} setIncarichi={setIncarichi} />
                                         <td className="text-center">
-                                            <Link to={`${i.classeDTO.id}/esercizi`}>
-                                                Esercizi
+                                            <Link to={`${i.classeDTO.id}/esercizi`} >
+                                                <button type="button" className="btn btn-secondary">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-journal" viewBox="0 0 16 16">
+                                                        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"></path>
+                                                        <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"></path>
+                                                    </svg>
+                                                    <span className="visually-hidden">Aggiungi esercizi</span>
+                                                </button>
                                             </Link>
                                         </td>
                                         <td className="text-center">
-                                            <Link to={`${i.classeDTO.id}/verifiche`}>
-                                                Verifiche
+                                            <Link to={`${i.classeDTO.id}/verifiche`} >
+                                                <button type="button" className="btn btn-secondary">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-journal-check" viewBox="0 0 16 16">
+                                                        <path fillRule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0"></path>
+                                                        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"></path>
+                                                        <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"></path>
+                                                    </svg>
+                                                <span className="visually-hidden">Aggiungi verifiche</span>
+                                                </button>
                                             </Link>
                                         </td>
                                     </tr>
